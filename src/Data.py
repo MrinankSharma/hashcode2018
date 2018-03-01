a92eaffb0fa3e2e63a8985429bb211caea62f2b1
MrinankSharma <mrinank.sharma.97+dev@gmail.com>
2018-03-01 19:11:18 +0000

    Add main file.

--- src/Data.py
+++ src/Data.py
@@ -53,3 +53,5 @@ class Vehicle:
 
     def __init__(self, position):
         self.position = position
+
+
---
+++ src/main.py
@@ -0,0 +1,14 @@
+
+original_data = None
+working_Data = None
+
+def load_data():
+    # TO DO - LOAD DATA
+    pass
+
+
+def run_simulation():
+
+
+if __name__ == "__main__":
+    pass
