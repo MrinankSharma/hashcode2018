db6b070c9c38acd106a2b889159a3cd7da2e59b0
MrinankSharma <mrinank.sharma.97+dev@gmail.com>
2018-03-01 20:20:36 +0000

    More skeletone.

--- src/main.py
+++ src/main.py
@@ -1,17 +1,29 @@
-
+from src.file_utils import read_data
 original_data = None
-working_Data = None
+working_data = None
 
-def load_data():
-    # TO DO - LOAD DATA
+def load_data(filename):
+    original_data = read_data(filename)
     pass
 
+# RAFI
 def cull_rides():
     pass
 
+# MRINANK
 def allocate_rides_to_cars():
+    # get assigned and unassigned cars, clear flags (set all to unassigned)
+    # get sorted routes
+    # for each sorted route
+    # assign the nearest unassigned car for each route, update the flag
     pass
 
+# Mrinank
+def update_status():
+    # if route position matches car position, busy
+    pass
+
+# Harry
 def update_positions():
     pass
 
@@ -24,10 +36,11 @@ def run_simulation():
         # allocate the free cars
         allocate_rides_to_cars()
         # update positions
+        update_status()
         update_positions()
         # increment timestep
         timestep+=1
 
 if __name__ == "__main__":
-    load_data()
+    load_data("data/a_example.in")
     run_simulation()
