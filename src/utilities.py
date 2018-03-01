61395f6f6dbef884d128b5c5a9fc811853c4264c
Elliot Fosong <efosong@gmx.com>
2018-03-01 20:26:59 +0000

    add priority functions

--- src/utilities.py
+++ src/utilities.py
@@ -2,12 +2,16 @@ def compute_l1_norm(location1, location2):
     '''Calculate the L1 norm between two points'''
     return abs(location1.x-location2.x) + abs(location1.y-location2.y)
 
-def ride_length(ridedata, vehicle):
-    '''Calculate the length of the ride from car's current location to
-    destination, via the start point'''
-    car_coord = vehicle.position
-    ride_start = ridedata.start
-    ride_end = ridedata.end
-    distance = compute_l1_norm(car_coord, ride_start) + \
-               compute_l1_norm(ride_start, ride_end)
-    return distance
+def ride_length(ride):
+    '''Calculate length of ride'''
+    return compute_l1_norm(ride.start, ride.end)
+
+def get_priority(ride, t):
+    '''Calculate the priorty for a ride, given the current timestep'''
+    bonus = B if ride.t < t else 0
+    return bonus + ride_length(ride)
+
+def sort_rides_by_priority(rides, t):
+    '''Given a list of rides, sort it in place'''
+    priority_map = lambda r: get_priority(r, t)
+    rides.sort(key=priority_map, reverse=True)
