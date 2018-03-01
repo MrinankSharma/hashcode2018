341bed809f360b4c107547f6f0842f8e66aa1195
MrinankSharma <mrinank.sharma.97+dev@gmail.com>
2018-03-01 20:06:08 +0000

    main loop skeleton.,

--- src/main.py
+++ src/main.py
@@ -6,9 +6,28 @@ def load_data():
     # TO DO - LOAD DATA
     pass
 
+def cull_rides():
+    pass
+
+def allocate_rides_to_cars():
+    pass
+
+def update_positions():
+    pass
 
 def run_simulation():
+    timestep = 0
 
+    while True:
+        # remove impossible rides
+        cull_rides()
+        # allocate the free cars
+        allocate_rides_to_cars()
+        # update positions
+        update_positions()
+        # increment timestep
+        timestep+=1
 
 if __name__ == "__main__":
-    pass
+    load_data()
+    run_simulation()
