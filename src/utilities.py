21f6321911aadd7bb119db6b83f273a56ed000e4
Elliot Fosong <efosong@gmx.com>
2018-03-01 18:47:07 +0000

    PEP8 compliancy utility.py

--- src/utilities.py
+++ src/utilities.py
@@ -1,11 +1,13 @@
-# Calculate the L1 norm between two points
 def compute_l1_norm(location1, location2):
+    '''Calculate the L1 norm between two points'''
     return abs(location1.x-location2.x) + abs(location1.y-location2.y)
 
-# Calculate the length of the ride from car's current location to destination, via the start point
 def ride_length(ridedata, vehicle):
+    '''Calculate the length of the ride from car's current location to
+    destination, via the start point'''
     car_coord = vehicle.position
     ride_start = ridedata.start
     ride_end = ridedata.end
-    distance = compute_l1_norm(car_coord, ride_start) + compute_l1_norm(ride_start, ride_end)
+    distance = compute_l1_norm(car_coord, ride_start) + \
+               compute_l1_norm(ride_start, ride_end)
     return distance
